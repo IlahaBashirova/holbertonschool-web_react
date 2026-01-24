01ae5558fbb40fd8de57cf35f27486d92c112a47
IlahaBashirova <11861@holbertonstudents.com>
2026-01-24 19:45:43 +0400

    done

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -3,8 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-
   entry: {
     all: [
       './modules/header/header.js',
@@ -12,48 +10,35 @@ module.exports = {
       './modules/footer/footer.js',
     ],
   },
-
   output: {
-    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public'),
+    filename: '[name].bundle.js',
   },
-
-  devtool: 'inline-source-map',
-
-  devServer: {
-    static: path.join(__dirname, 'public'),
-    compress: true,
-    port: 8564,
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'], // ✅ correct order
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
-
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({ title: 'Holberton Dashboard' }),
   ],
-
-  optimization: {
-    splitChunks: { chunks: 'all' },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    port: 8564,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: { bypassOnDebug: true, disable: true },
+          },
+        ],
+      },
+    ],
   },
 };
