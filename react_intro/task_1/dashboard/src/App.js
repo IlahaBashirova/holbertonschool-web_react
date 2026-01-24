c1a7850e23e18c0aca4efef515c54a66c306ea39
IlahaBashirova <11861@holbertonstudents.com>
2026-01-24 21:25:33 +0400

    done

--- react_intro/task_1/dashboard/src/App.js
+++ react_intro/task_1/dashboard/src/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Notifications from './Notifications';
 
 function App() {
   return (
     <div className="App">
-        <div className="App-header">
+      <Notifications />
+
+      <div className="App-header">
         <img src={logo} alt="logo" />
         <h1>School dashboard</h1>
       </div>
+
       <div className="App-body">
-        <p>
-          Login to access the full dashboard
-        </p>
+        <p>Login to access the full dashboard</p>
       </div>
+
       <div className="App-footer">
         <p>Copyright 2020 - holberton School</p>
       </div>
