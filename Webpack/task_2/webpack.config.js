c5837da2b951ddbe7058bc01e8b2e078618e6248
IlahaBashirova <11861@holbertonstudents.com>
2026-01-24 18:35:49 +0400

    done

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -8,25 +8,4 @@ module.exports = {
     publicPath: '/public'
   },
   mode: 'production',
-  module: {
-	rules: [
-		{
-			test: /\.css$/i,
-			use: ["css-loader", "style-loader"],
-		},
-		{
-			test: /\.(gif|png|jpe?g|svg)$/i,
-			use: [
-				"file-loader",
-				{
-					loader: "image-webpack-loader",
-					options: {
-							bypassingOnDebug: true,
-							disable: true,
-					},
-				},
-			],
-		},
-	],
-},
 };
