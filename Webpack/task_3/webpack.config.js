90a67501135fb530bae8fa54bd0ae00fa32cc16f
IlahaBashirova <11861@holbertonstudents.com>
2026-01-24 19:54:19 +0400

    done

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -3,42 +3,51 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    all: [
-      './modules/header/header.js',
-      './modules/body/body.js',
-      './modules/footer/footer.js',
-    ],
+	// entry: {
+	// 	// lodash: './node_modules/lodash',
+	// 	// jquery: './node_modules/jquery',
+	// 	header: {
+	// 		import: './modules/header/header.js',
+	// 	},
+	// 	body: {
+	// 		import: './modules/body/body.js',
+	// 		// dependOn: 'jquery',
+	// 	},
+	// 	footer: {
+	// 		import: './modules/footer/footer.js',
+	// 	},
+	// 	// shared: 'lodash',
+	// },
+	entry: {
+    all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
   },
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[name].bundle.js',
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ title: 'Holberton Dashboard' }),
-  ],
-  devtool: 'inline-source-map',
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'public'),
-    },
-    port: 8564,
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: { bypassOnDebug: true, disable: true },
-          },
-        ],
-      },
-    ],
-  },
+	output: {
+		path: path.resolve(__dirname, './public'),
+		filename: '[name].bundle.js',
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+      title: 'Holberton Dashboard',
+    }),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+				use: ['file-loader'],
+			},
+		],
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './public',
+		port: 8564,
+	},
+	mode: 'development',
 };
