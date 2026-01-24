16fb90087ce9651ac1dfd6a252f89ba76cc638e3
IlahaBashirova <11861@holbertonstudents.com>
2026-01-24 19:12:34 +0400

    done

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -3,44 +3,42 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: {
-		all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
-	  },  output: {
+  entry: {
+    all: [
+      './modules/header/header.js',
+      './modules/body/body.js',
+      './modules/footer/footer.js',
+    ],
+  },
+  output: {
     path: path.resolve(__dirname, 'public'),
-	filename: '[name].bundle.js',
-},
-plugins: [
-	new CleanWebpackPlugin(),
-	new HtmlWebpackPlugin({
-	title: 'Holberton Dashboard',
-    }),
-	],
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './public',
-		port: 8564,
-	},
-
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ title: 'Holberton Dashboard' }),
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    port: 8564,
+  },
   mode: 'development',
   module: {
-	rules: [
-		{
-			test: /\.css$/i,
-			use: ["css-loader", "style-loader"],
-		},
-		{
-			test: /\.(gif|png|jpe?g|svg)$/i,
-			use: [
-				"file-loader",
-				{
-					loader: "image-webpack-loader",
-					options: {
-							bypassingOnDebug: true,
-							disable: true,
-					},
-				},
-			],
-		},
-	],
-},
+    rules: [
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: { bypassOnDebug: true, disable: true },
+          },
+        ],
+      },
+    ],
+  },
 };
