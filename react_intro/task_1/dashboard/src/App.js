b030610be8dfa556789eaab1a8d65722916e8c68
IlahaBashirova <11861@holbertonstudents.com>
2026-01-24 21:42:18 +0400

    done

--- react_intro/task_1/dashboard/src/App.js
+++ react_intro/task_1/dashboard/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Notifications from './Notifications';
+import React from "react";
+import "./App.css";
+import holberton_logo from "./holberton_logo.jpg";
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
-    <div className="App">
-      <Notifications />
-
+    <>
       <div className="App-header">
-        <img src={logo} alt="logo" />
+        <img src={holberton_logo} />
         <h1>School dashboard</h1>
       </div>
-
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-
       <div className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
       </div>
-    </div>
+    </>
   );
 }
 
