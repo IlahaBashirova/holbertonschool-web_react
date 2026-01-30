5a1de7a63ffbcfb584eff15049913a36503a4f9d
IlahaBashirova <11861@holbertonstudents.com>
2026-01-30 20:18:49 +0400

    done

--- react_styling/task_2/dashboard/src/Notifications/NotificationItem.spec.js
+++ react_styling/task_2/dashboard/src/Notifications/NotificationItem.spec.js
@@ -1,25 +1,23 @@
 import NotificationItem from "./NotificationItem";
 import { render, screen, fireEvent } from "@testing-library/react";
 
-test('Check whether the li element has the color blue, and the the attribute data-notification-type set to default', () => {
+test("Check whether the li element has the attribute data-notification-type set to default", () => {
   render(<NotificationItem type="default" value="Test notification" />);
-  const li = screen.getByText('Test notification');
+  const li = screen.getByText("Test notification");
 
   expect(li).toBeInTheDocument();
-  expect(li).toHaveAttribute('data-notification-type', 'default');
-  expect(li).toHaveStyle('color: blue');
+  expect(li).toHaveAttribute("data-notification-type", "default");
 });
 
-test('Check whether the li element has the color red, and the the attribute data-notification-type set to urgent', () => {
+test("Check whether the li element has the attribute data-notification-type set to urgent", () => {
   render(<NotificationItem type="urgent" value="Test urgent notification" />);
-  const li = screen.getByText('Test urgent notification');
+  const li = screen.getByText("Test urgent notification");
 
   expect(li).toBeInTheDocument();
-  expect(li).toHaveAttribute('data-notification-type', 'urgent');
-  expect(li).toHaveStyle('color: red');
+  expect(li).toHaveAttribute("data-notification-type", "urgent");
 });
 
-test('calls markAsRead with correct id on click', () => {
+test("calls markAsRead with correct id on click", () => {
   const mockMarkAsRead = jest.fn();
   render(
     <NotificationItem
@@ -30,7 +28,7 @@ test('calls markAsRead with correct id on click', () => {
     />
   );
 
-  const li = screen.getByText('Clickable notification');
+  const li = screen.getByText("Clickable notification");
   fireEvent.click(li);
 
   expect(mockMarkAsRead).toHaveBeenCalledWith(42);
