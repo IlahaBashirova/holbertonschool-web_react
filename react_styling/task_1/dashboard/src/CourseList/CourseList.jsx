6f3c6b66c624b2c84289a2a8117c89cea82aa327
IlahaBashirova <11861@holbertonstudents.com>
2026-01-30 19:31:30 +0400

    done

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -10,33 +10,30 @@ class CourseList extends React.Component {
     const { courses } = this.props;
 
     return (
-      <div className="w-4/5 mx-auto my-8">
+      <div className="w-4/5 mx-auto">
         <table id="CourseList" className="w-full border-collapse">
-          {courses.length > 0 ? (
-            <>
-              <thead>
-                <CourseListRow isHeader={true} textFirstCell="Available courses" />
+          <thead>
+            <CourseListRow isHeader={true} textFirstCell="Available courses" />
+            <CourseListRow
+              isHeader={true}
+              textFirstCell="Course name"
+              textSecondCell="Credit"
+            />
+          </thead>
+
+          <tbody>
+            {courses.length > 0 ? (
+              courses.map((course) => (
                 <CourseListRow
-                  isHeader={true}
-                  textFirstCell="Course name"
-                  textSecondCell="Credit"
+                  key={course.id}
+                  textFirstCell={course.name}
+                  textSecondCell={course.credit}
                 />
-              </thead>
-              <tbody>
-                {courses.map((course) => (
-                  <CourseListRow
-                    key={course.id}
-                    textFirstCell={course.name}
-                    textSecondCell={course.credit}
-                  />
-                ))}
-              </tbody>
-            </>
-          ) : (
-            <tbody>
+              ))
+            ) : (
               <CourseListRow isHeader={true} textFirstCell="No course available yet" />
-            </tbody>
-          )}
+            )}
+          </tbody>
         </table>
       </div>
     );
