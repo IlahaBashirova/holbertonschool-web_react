fcd7c245fa26aeedef8060ad39ddfe3fb5ffa191
IlahaBashirova <11861@holbertonstudents.com>
2026-01-30 18:51:29 +0400

    done

--- react_styling/task_0/dashboard/src/main.jsx
+++ react_styling/task_0/dashboard/src/main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App/App.jsx";
 
@@ -8,8 +7,4 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./main.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById("root")).render(<App />);
